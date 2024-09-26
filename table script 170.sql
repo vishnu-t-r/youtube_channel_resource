439c253c52b17431659d30187fb893c91a5a80c8
Vishnu <109589119+vishnu-t-r@users.noreply.github.com>
2024-09-26 11:01:44 +0530

    table script 170

---
+++ table script 170.sql
@@ -0,0 +1,21 @@
+Create Table lift_passengers(
+Passenger_Name Varchar(20),
+Weight_Kg int,
+Lift_Id int)
+
+Insert into Lift_Passengers Values('Lewis',85,1)
+Insert into Lift_Passengers Values('Anto',73,1)
+Insert into Lift_Passengers Values('Danny',95,1)
+Insert into Lift_Passengers Values('Mary',80,1)
+Insert into Lift_Passengers Values('Raj',92,1)
+Insert into Lift_Passengers Values('Mark',83,2)
+Insert into Lift_Passengers Values('Robert',77,2)
+Insert into Lift_Passengers Values('Maria',73,2)
+Insert into Lift_Passengers Values('Susan',85,2)
+Insert into Lift_Passengers Values('John',92,2)
+
+Create Table Lift( Id int,
+Capacity_Kg Bigint)
+
+Insert into Lift Values(1,300)
+Insert into Lift Values(2,350)
