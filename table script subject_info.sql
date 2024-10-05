5a24409dfd28e9be650cae5d2c15bf4c4e424bd0
Vishnu <109589119+vishnu-t-r@users.noreply.github.com>
2024-10-05 15:08:07 +0530

    table script subject_info

---
+++ table script subject_info.sql
@@ -0,0 +1,19 @@
+/*
+create table subject_info(
+class varchar(20),
+subject varchar(20))
+
+insert into subject_info(class, subject)
+values('A','Maths'),
+('B','English'),
+('A','Social'),
+('B','Chemistry'),
+('A','Physics'),
+('B','Statistics'),
+('A','Biology'),
+('B','Maths'),
+('A','Science'),
+('B','Social')
+
+select * from subject_info;
+*/
