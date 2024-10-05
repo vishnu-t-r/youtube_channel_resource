f62ef6e315b1f3e2925419b617135ab445c9c908
Vishnu <109589119+vishnu-t-r@users.noreply.github.com>
2024-10-05 15:28:24 +0530

    table script walmart

---
+++ table script walmart.sql
@@ -0,0 +1,25 @@
+/*
+CREATE TABLE walmart (
+    ProductID INT PRIMARY KEY,
+    Name VARCHAR(50),
+    Sales int
+);
+
+
+
+-- Insert data into the table
+INSERT INTO walmart (ProductID, Name, Sales) VALUES
+(1, 'Laptop', 7500),
+(2, 'Smartphone', 6000),
+(3, 'Furniture', 2500),
+(4, 'Headphones', 800),
+(5, 'Smartwatch', 700),
+(6, 'Books', 500),
+(7, 'Keyboard', 300),
+(8, 'Bags', 200),
+(9, 'Charger', 200),
+(10, 'Toys', 100),
+(11, 'Peronal Care', 100);
+*/
+
+--select * from walmart;
