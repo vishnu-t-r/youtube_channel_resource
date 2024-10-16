26ef268a72de6a68f4b7312c394d0c4c33c640d4
Vishnu <109589119+vishnu-t-r@users.noreply.github.com>
2024-10-16 11:21:38 +0530

    table script src_dest

---
+++ table script src_dest.sql
@@ -0,0 +1,23 @@
+--table
+--src_dest table
+
+/*
+create table src_dest(
+source varchar(50),
+destination varchar(50),
+distance int
+)
+
+insert into src_dest(source,destination,distance)
+values('Alaska','Albany',5166),
+('Dover','Florida',1393),
+('Illinois','Indiana',279),
+('New Mexico','New York',2873),
+('Albany','Alaska',5166),
+('Ohio','Oklahoma',1383),
+('Indiana','Illinois',279),
+('Oklahoma','Ohio',1383),
+('Frankfort','Georgia',695),
+('Georgia','Frankfort',695)
+
+*/
