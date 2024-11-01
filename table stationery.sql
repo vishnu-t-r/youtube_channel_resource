285936ad852940e929ae697688b4b5d2f493e735
Vishnu <109589119+vishnu-t-r@users.noreply.github.com>
2024-11-01 20:59:19 +0530

    table stationery

---
+++ table stationery.sql
@@ -0,0 +1,23 @@
+use int_ques;
+
+/*
+Create Table STATIONERY(
+Category VARCHAR(20),
+Brand VARCHAR(20),
+Product_Name VARCHAR(20),
+Price int,
+Primary Key(Product_Name));
+
+INSERT INTO STATIONERY VALUES('Pen','Alpha','Alpen',280),
+('Pen','Fabre','Fapen',250),
+('Pen','Camel','Capen',220),
+('Board','Alpha','Alord',550),
+('Board','Fabre','Faord',400),
+('Board','Camel','Carod',250),
+('Notebook','Alpha','Albook',250),
+('Notebook','Fabre','Fabook',230),
+('Notebook','Camel','Cabook',210);
+*/
+
+--select * from STATIONERY;
+
