3dd3a6d4258d0cd228073cfd2ad47d97d4c8a7d1
Vishnu <109589119+vishnu-t-r@users.noreply.github.com>
2024-11-20 11:58:16 +0530

    table script cities

---
+++ table script cities.sql
@@ -0,0 +1,16 @@
+/*
+CREATE TABLE Cities (
+    CityID INT,
+    CityName VARCHAR(50),
+    NeighborCityID INT,
+    Distance INT
+);
+
+INSERT INTO Cities (CityID, CityName, NeighborCityID, Distance) VALUES 
+(1, 'New York', 2, 200),
+(1, 'New York', 3, 300),
+(2, 'Boston', 1, 200),
+(2, 'Boston', 3, 150),
+(3, 'Chicago', 1, 280),
+(3, 'Chicago', 2, 150);
+*/
