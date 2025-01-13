0b14c98f5d479ad76b9420e389eec7541bbb122c
Vishnu <109589119+vishnu-t-r@users.noreply.github.com>
2025-01-13 11:34:50 +0530

    table script cube_sales

---
+++ table script cube_sales.sql
@@ -0,0 +1,13 @@
+/*
+create table cube_sales(
+product varchar(20),
+region varchar(25),
+salesamount int
+);
+
+insert into cube_sales(product, region, salesamount)
+values('A',	'North',	100),
+('A',	'South',	150),
+('B',	'North',	200),
+('B',	'South',	250)
+*/
