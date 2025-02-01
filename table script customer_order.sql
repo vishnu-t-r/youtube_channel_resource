33242489f46ae43c7d92c2640f34659ae128eb9d
Vishnu <109589119+vishnu-t-r@users.noreply.github.com>
2025-02-01 14:48:38 +0530

    table script customer_order

---
+++ table script customer_order.sql
@@ -0,0 +1,20 @@
+/*
+CREATE TABLE Customer_Order (
+    CustomerID INT,
+    OrderDate DATE,
+    OrderID INT PRIMARY KEY
+);
+
+INSERT INTO Customer_Order (CustomerID, OrderDate, OrderID)
+VALUES
+(1, '2025-01-01', 101),
+(1, '2025-01-02', 102),
+(1, '2025-01-04', 103),
+(1, '2025-01-05', 104),
+(1, '2025-01-07', 105),
+(2, '2025-01-01', 106),
+(2, '2025-01-02', 107),
+(2, '2025-01-03', 108),
+(3, '2025-01-01', 109),
+(3, '2025-01-03', 110);
+*/
