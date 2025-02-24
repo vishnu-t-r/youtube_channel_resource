9a8045af06eb19d4c289a634d9cc9089194caaa7
Vishnu <109589119+vishnu-t-r@users.noreply.github.com>
2025-02-24 10:19:21 +0530

    table script orders_transactions

---
+++ table script orders_transactions.sql
@@ -0,0 +1,15 @@
+/*
+CREATE TABLE orders_transactions (
+    transaction_id INT PRIMARY KEY,
+    customer_id INT,
+    sale_amount DECIMAL(10,2)
+);
+
+INSERT INTO orders_transactions (transaction_id, customer_id, sale_amount) VALUES
+(201, 101, 250),
+(202, 102, 750),
+(203, 101, 600),
+(204, 103, 400),
+(205, 102, 180),
+(206, 103, 900);
+*/
