02ebacaeae4a31e85062f1ae3993e2acce744a4a
Vishnu <109589119+vishnu-t-r@users.noreply.github.com>
2025-04-10 23:29:40 +0530

    Create table_script student_scores.sql

---
+++ table_script student_scores.sql
@@ -0,0 +1,16 @@
+/*
+CREATE TABLE student_scores (
+    student_id INT,
+    subject VARCHAR(50),
+    score INT
+);
+
+
+INSERT INTO student_scores (student_id, subject, score) VALUES
+(1, 'Math', 85),
+(1, 'Science', 92),
+(2, 'Math', 90),
+(2, 'Science', 88),
+(3, 'Math', 78),
+(3, 'Science', 78);
+*/
