13fac5a0c322b787481a416dd964ee97128ee38e
Vishnu <109589119+vishnu-t-r@users.noreply.github.com>
2025-04-16 23:16:52 +0530

    Create table script_sales_info.sql

---
+++ table script_sales_info.sql
@@ -0,0 +1,12 @@
+
+/*
+CREATE TABLE Sales_Info (
+    SaleDate DATE PRIMARY KEY,
+    Amount INT
+);
+
+INSERT INTO Sales_Info (SaleDate, Amount) VALUES
+('2099-01-01', 100),
+('2099-01-03', 200),
+('2099-01-06', 150);
+*/
