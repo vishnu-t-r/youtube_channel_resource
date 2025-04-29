456dc949b5f35d3ea3dc2ef0ccf82336ea91b283
Vishnu <109589119+vishnu-t-r@users.noreply.github.com>
2025-04-29 23:10:54 +0530

    Create table script orders_info.sql

---
+++ table script orders_info.sql
@@ -0,0 +1,19 @@
+
+/*
+CREATE TABLE Orders_Info (
+    order_id INT PRIMARY KEY,
+    customer_id INT,
+    order_date DATE,
+    amount DECIMAL(10, 2)
+);
+
+
+INSERT INTO Orders_Info (order_id, customer_id, order_date, amount) VALUES
+(501, 1, '2099-01-10', 300),
+(502, 2, '2099-01-15', 500),
+(503, 1, '2099-02-12', 400),
+(504, 1, '2099-03-10', 600),
+(505, 2, '2099-02-18', 450),
+(506, 2, '2099-03-20', 470),
+(507, 1, '2099-04-21', 700);
+*/
