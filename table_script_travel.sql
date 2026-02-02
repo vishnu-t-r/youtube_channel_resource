4584e8ced0f8caaf3c56033e2e303c5dff761807
Vishnu <109589119+vishnu-t-r@users.noreply.github.com>
2026-02-02 23:54:30 +0530

    Create table_script_travel.sql

---
+++ table_script_travel.sql
@@ -0,0 +1,17 @@
+/*
+-- Create the Table
+CREATE TABLE travel (
+    source VARCHAR(50),
+    destination VARCHAR(50),
+);
+
+-- Insert Sample Data
+INSERT INTO travel (Source, Destination)
+VALUES 
+('Delhi', 'Mumbai'),
+('Mumbai', 'Delhi'),
+('New York', 'London'),
+('London', 'New York'),
+('Paris', 'Tokyo'),
+('Chicago', 'Denver'); -- Non-paired entry
+*/
